9e007af9ba3705418828241f24dac98cc3245d3b
Nenenj <nnennanjoku08@gmail.com>
2022-11-30 06:43:56 -0800

    9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -7,5 +7,5 @@ def print_last_digit(number):
     else:
         number = number % 10
 
-        print("{:d}".format(number), end="")
-        return number
+    print("{:d}".format(number), end="")
+    return number
