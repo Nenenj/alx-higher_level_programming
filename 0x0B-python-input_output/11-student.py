ce3140882db74d2d61b95927b8b26dde0d51f6ec
Nenenj <nnennanjoku08@gmail.com>
2023-01-10 04:49:21 -0800

    11-student.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -26,7 +26,7 @@ class Student:
         if (type(attrs) == list and
                 all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        return self.__dict__i
+        return self.__dict__
 
     def reload_from_json(self, json):
         """Replace all attributes of the Student.
