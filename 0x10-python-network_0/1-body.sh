32dfa6ccb6a8c42a441eff09400fa8d1e21fedfa
Nenenj <nnennanjoku08@gmail.com>
2024-03-28 22:14:00 +0100

    update

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,20 +1,3 @@
 #!/bin/bash
-
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-url=$1
-
-response=$(curl -s -w '%{http_code}' "$url")
-
-status_code=$(echo "$response" | tail -n 1)
-response_body=$(echo "$response" | head -n -1)
-
-if [ "$status_code" -eq 200 ]; then
-    echo "Response Body:"
-    echo "$response_body"
-else
-    echo "Request returned status code: $status_code"
-fi
+# Takes URL, sends a Get request to thr url,and displays response
+curl -Lsf "$1"
