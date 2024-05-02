18a80801770c6cd9524c672df5b2b0647d68d2a3
Nenenj <nnennanjoku08@gmail.com>
2024-05-02 16:25:29 +0100

    javascript-web_jquery

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+	<html>
+	<head>
+	<title>Update Text Color</title>
+	<style>
+	header {
+		color: black;
+	}
+	</style>
+	</head>
+	<body>
+	<header>This is the header text</header>
+
+	<script>
+	// Select the <header> element
+	const headerElement = document.querySelector('header');
+	
+	// Update the text color to red
+        headerElement.style.color = '#FF0000';
+	</script>
+	</body>
+	</html>
